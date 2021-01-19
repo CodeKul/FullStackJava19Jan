259a421005aece7d91159182d8de99d6c6d2ec31
vaibhav <codekul.vaibhav@gmail.com>
2021-01-19 12:18:53 +0530

    First Commit

---
+++ src/DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+
+    //primitive datatypes
+    int i = 0;
+    char j = 'c';
+    boolean b = false;
+    float f = 1.0f;
+
+    //non primitive datatypes
+
+    String s = "Akshay";
+    Integer q = 0;
+    Double d = 1.0;
+    Character c ='d';
+
+    static void getInfo(){
+        System.out.println("hii");
+    }
+
+     static public void main(String...args) {
+
+        DataTypes.getInfo();
+    }
+
+}
