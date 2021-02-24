c5f4c0d3ab0a6b745129ba3e37741f273b8a3b14
vaibhav <codekul.vaibhav@gmail.com>
2021-02-24 12:23:57 +0530

    Insert,update delete and Prepared Statement demo

--- src/jdbc/InsertDemo.java
+++ src/jdbc/InsertDemo.java
@@ -2,8 +2,8 @@ package jdbc;
 
 import java.sql.*;
 
-public class JdbcDemo {
-
+public class InsertDemo {
+    //
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -11,21 +11,20 @@ public class JdbcDemo {
                     DriverManager.getConnection("jdbc:mysql://localhost:3306/java19jan?characterEncoding=latin1", "root", "password");
             Statement statement = connection.createStatement();
 
+            String insert = "Insert into employee(id,name,address) values(2,'Sachin','Nashik')";
+            String update = "update employee set name ='rahul' where id=2";
+            String delete = "delete from employee where id=2";
 
-            ResultSet rs = statement.executeQuery("select * from employee");
+            int i = statement.executeUpdate(delete);
 
-//            executeQuery -> it is use for select statement
-            while (rs.next()) {
-                System.out.println(rs.getInt(1));
-                System.out.println(rs.getString(2));
-                System.out.println(rs.getString(3));
-            }
+//            executeUpdate -> it is used for insert update delete
+
+            System.out.println("row inserted=>"+i);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-
 
     }
 }
---
+++ src/jdbc/PreparedStatDemo.java
@@ -0,0 +1,40 @@
+package jdbc;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class PreparedStatDemo {
+
+    public static void main(String[] args) {
+        try {
+            int id;
+            String name;
+            String address;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter id=>");
+            id = scanner.nextInt();
+            System.out.println("Enter Name->");
+            name = scanner.next();
+            System.out.println("Enter Address");
+            address = scanner.next();
+
+            String insert = "Insert into employee(id,name,address) values(?,?,?)";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/java19jan?characterEncoding=latin1", "root", "password");
+            PreparedStatement statement = connection.prepareStatement(insert);
+
+            statement.setInt(1,id);
+            statement.setString(2,name);
+            statement.setString(3,address);
+
+            statement.executeUpdate();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+}
